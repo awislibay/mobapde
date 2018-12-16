2d202985f49caf77a0c4de7e2fe53f70d9b07eb3
awislibay <ayawisleebai12@gmail.com>
2018-12-17 00:25:06 +0800

    YEET

---
+++ REM/Assets/Character/Player/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    private Vector2 velocity;
+
+    public float SmoothTimeY;
+    public float SmoothTimeX;
+
+    public GameObject player;
+
+
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, SmoothTimeX);
+        float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, SmoothTimeY);
+
+        transform.position = new Vector3(posX, posY, transform.position.z);
+    }
+}
---
+++ REM/Assets/NextScene.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+ 
+ 
+ 
+ 
+public class NextScene : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collider", gameObject);
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Graveyard", LoadSceneMode.Single);
+        }
+    }
+}
